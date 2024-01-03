91116a3c66d48c42ca27e5fee7a845239f986b9f
ADMIN <ADMIN@DESKTOP-PNQ9MU0>
2024-01-03 18:58:59 +0530

    Add project files.

---
+++ Api/Data/Datacontext.cs
@@ -0,0 +1,19 @@
+﻿using Api.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class Datacontext : DbContext
+    {
+        public Datacontext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+
+
+
+
+
+    }
+    
+}
---
+++ Api/Entites/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Entites
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
